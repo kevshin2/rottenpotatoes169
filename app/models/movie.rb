fd5bc18a125549bfeaf41b9c9e2c99f507854880
kevshin2 <kevinshen34@gmail.com>
2013-10-06 21:53:35 -0700

    y

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,19 @@
 class Movie < ActiveRecord::Base
 	def self.rating
-		%w(G PG PG-13 NC-17 R)
+		ratings = []
+		Movie.all.each do |movie|
+			unless ratings.include?(movie.rating)
+				ratings << movie.rating
+			end
+		end
+		if ratings.include?("NC-17")
+			ratings.delete("NC-17")
+			ratings.sort!
+			ratings << ("NC-17")
+			return ratings
+		else
+			ratings.sort! 
+			return ratings
+		end
 	end
 end
